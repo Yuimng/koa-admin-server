f9cee12bea369c696bd3cfc627a4062927a2d3bf
yuimng <944307891@qq.com>
2024-06-13 17:33:05 +0800

    初始菜单数据

--- src/models/sync.ts
+++ src/models/sync.ts
@@ -3,6 +3,8 @@ import sequelize from '../config/mysql'
 import roleModel from './role.model'
 import userModel from './user.model'
 import userRoleModel from './userRole.model'
+import menuModel from './menu.model'
+import roleMenuModel from './roleMenu.model'
 
 import { ADMIN_CONFIG } from '../config'
 
@@ -31,6 +33,109 @@ const synchonize = async () => {
   await userRoleModel.create({
     userId: newUser.dataValues.id,
     roleId: adminRole.dataValues.id,
+  })
+
+  // 初始菜单数据
+  const menu1 = await menuModel.create({
+    name: 'dashboard',
+    path: '/dashboard',
+    parentId: 0,
+    sort: 1,
+    title: '主面板',
+    icon: 'zhuye',
+    isLink: 0,
+    isEnable: 1,
+    isAffix: 1,
+    isKeepAlive: 1,
+  })
+  await roleMenuModel.create({
+    roleId: adminRole.dataValues.id,
+    menuId: menu1.dataValues.id,
+  })
+
+  const menu2 = await menuModel.create({
+    name: 'system',
+    path: '/system',
+    parentId: 0,
+    sort: 1,
+    title: '系统管理',
+    icon: 'shezhi',
+    isLink: 0,
+    isEnable: 1,
+    isAffix: 0,
+    isKeepAlive: 1,
+  })
+  await roleMenuModel.create({
+    roleId: adminRole.dataValues.id,
+    menuId: menu2.dataValues.id,
+  })
+
+  const menu3 = await menuModel.create({
+    name: 'accountManage',
+    path: '/system/accountManage',
+    parentId: menu2.dataValues.id,
+    sort: 1,
+    title: '账号管理',
+    icon: 'yingyong',
+    isLink: 0,
+    isEnable: 1,
+    isAffix: 0,
+    isKeepAlive: 1,
+  })
+  await roleMenuModel.create({
+    roleId: adminRole.dataValues.id,
+    menuId: menu3.dataValues.id,
+  })
+
+  const menu4 = await menuModel.create({
+    name: 'roleManage',
+    path: '/system/roleManage',
+    parentId: menu2.dataValues.id,
+    sort: 1,
+    title: '角色管理',
+    icon: 'yingyong',
+    isLink: 0,
+    isEnable: 1,
+    isAffix: 0,
+    isKeepAlive: 1,
+  })
+  await roleMenuModel.create({
+    roleId: adminRole.dataValues.id,
+    menuId: menu4.dataValues.id,
+  })
+
+  const menu5 = await menuModel.create({
+    name: 'menuManage',
+    path: '/system/menuManage',
+    parentId: menu2.dataValues.id,
+    sort: 1,
+    title: '菜单管理',
+    icon: 'yingyong',
+    isLink: 0,
+    isEnable: 1,
+    isAffix: 0,
+    isKeepAlive: 1,
+  })
+  await roleMenuModel.create({
+    roleId: adminRole.dataValues.id,
+    menuId: menu5.dataValues.id,
+  })
+
+  const menu6 = await menuModel.create({
+    name: 'departmentManage',
+    path: '/system/departmentManage',
+    parentId: menu2.dataValues.id,
+    sort: 1,
+    title: '部门管理',
+    icon: 'yingyong',
+    isLink: 0,
+    isEnable: 1,
+    isAffix: 0,
+    isKeepAlive: 1,
+  })
+  await roleMenuModel.create({
+    roleId: adminRole.dataValues.id,
+    menuId: menu6.dataValues.id,
   })
 }
 
