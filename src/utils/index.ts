54668aaff7e82d47ff2b0ae6e87d36114f20c316
yuimng <944307891@qq.com>
2024-06-19 17:24:59 +0800

    构建树形菜单  解决搜索结果遗漏问题

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -41,21 +41,37 @@ export const formatMenus = (menus: any[]) => {
   })
   // 排序
   menuList.sort((a, b) => a.sort - b.sort)
-  // 将格式化的所有菜单根据 parentId 构建嵌套菜单
-  return buildNestedMenus(menuList)
+  // 将排序好的所有菜单根据 parentId 转为树形菜单
+  return buildTreeMenu(menuList)
 }
-
 /**
- * @description: 构建嵌套菜单
- * @param {Menu[]} menus
- * @param {number} parentId 顶层父级菜单 parentId 为 0
- * @return {*}
+ * 构建树形菜单
+ * @param menus Menu[] 平铺菜单
+ * @returns Menu[] 树形菜单
  */
-function buildNestedMenus(menus: Menu[], parentId = 0): Menu[] {
-  return menus
-    .filter((menu) => menu.parentId === parentId) // 选中 parrentId 为 0 的菜单
-    .map((menu) => ({
-      ...menu, // 放入父级菜单
-      children: buildNestedMenus(menus, menu.id), // 识别parentId 为 menu.id 的子菜单
-    }))
+function buildTreeMenu(menus: Menu[]): Menu[] {
+  const menuMap: { [id: number]: Menu } = {} // 使用索引签名定义对象类型
+  const roots: Menu[] = [] // 根节点数组类型明确为Menu[]
+  // 预处理：将所有菜单项放入map中，便于查找
+  menus.forEach((menu) => {
+    menu.children = [] // 初始化children属性，即使没有子菜单也是个空数组
+    menuMap[menu.id] = menu
+  })
+  // 遍历菜单，构建树结构
+  menus.forEach((menu) => {
+    if (menu.parentId !== 0) {
+      // 查找父级菜单，有则将当前菜单放入父级菜单的children中
+      const parent = menuMap[menu.parentId]
+      if (parent) {
+        parent.children.push(menu)
+      } else {
+        // 找不到父级菜单，说明有孤立菜单，将其放入根节点数组中（搜索的时候可能出现）
+        roots.push(menu)
+      }
+    } else {
+      // parentId为0，说明是根节点
+      roots.push(menu)
+    }
+  })
+  return roots // 返回根节点数组，即构建好的树形菜单
 }
